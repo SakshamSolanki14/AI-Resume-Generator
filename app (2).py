9925d42b28bf11f3f1968dfb4e8217a9d0d8ba63
SakshamSolanki14 <saksham.14solanki@gmail.com>
2026-07-28 13:31:23 +0530

    Update app (2).py

--- app (2).py
+++ app (2).py
@@ -137,20 +137,20 @@ query = final_prompt+user_details
 
 import base64
 
-OPTIONS = ["Delhi","Noida","Gurugram",
-          "Kanpur","Lucknow","Banglore","Pune"]
-LOCATIONS = st.sidebar.multiselect('SELECT LOCATIONS: ',
-                                  optons = OPTIONS)
-JOB_PROFILE = ["Python Developer","GEN AI",
-              "Full Stack Developer","Data Analyst"]
+OPTIONS = ["DELHI", "NOIDA", "GURGAON/GURUGRAM",
+'KANPUR', 'LUCKNOW', 'BANGLORE', 'PUNE']
+LOCATION = st.sidebar.multiselect('SELECT LOCATION: ',
+                                  options = OPTIONS)
+JOB_PROFILE = ["PYTHON DEVELOPER", 'GEN AI',
+'FULL-STACK DEVELOPER', 'DATA ANALYST']
 PROFILE = st.sidebar.multiselect("SELECT JOB ROLE",
-                                options = JOB_PROFILE)
+options = JOB_PROFILE)
 job_prompt = f"""Based on {PROFILE} jobs in {LOCATION}, I
 want latest job news in using tavily,
 try top 10 search or whatever available
 and give result like naukri theme design with
 job name, job desc, salary,
-apply link and output must be in html no markdown"""
+apply link and output must be in html no markdown""" 
 
 if st.button('generate resume'):
   with st.spinner("runnign agent"):
