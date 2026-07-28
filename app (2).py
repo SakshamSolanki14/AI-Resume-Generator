044d654958c4cab56fd5c572b1b6611d97eefe1d
SakshamSolanki14 <saksham.14solanki@gmail.com>
2026-07-28 12:14:11 +0530

    Update app (2).py

--- app (2).py
+++ app (2).py
@@ -22,12 +22,19 @@ from langchain.messages import SystemMessage, HumanMessage
 import numpy as np
 import streamlit as st
 from langchain_community.document_loaders import PyMuPDFLoader
+from PIL import Image
 
 # ===========API KEY LOAD=================
 
 GOOGLE_API_KEY = st.sidebar.text_input("GOOGLE_API_KEY",type="password")
 GROQ_API_KEY = st.sidebar.text_input("GROQ_API_KEY",type="password")
 TAVILY_API_KEY = st.sidebar.text_input("TAVILY_API_KEY",type="password")
+
+if not (GOOGLE_API_KEY) and not (GROQ_API_KEY) and not (TAVILY_API_KEY):
+    st.sidebar.warning("Pass API Keys")
+    st.stop()
+else:
+    st.success("API Keys Loaded")
 
 #==========MODEL BUILDING===============
 model = ChatGoogleGenerativeAI(
@@ -87,6 +94,28 @@ def resume_maker_prompt():
     prompt = f.read()
   return prompt
 resume_maker_prompt()
+
+
+# ===============UPLOAD IMAGE======================
+uploaded_file = st.sidebar.file_uploader(
+    "Choose an image file",
+    type=["jpg","jpeg","png","webp"]
+)
+if uploaded_file is not None:
+    try:
+        image = Image.open(uploaded_file)
+        st.sidebar.image(image, caption="Uploaded Image", use_container_width=True)
+        if image.mode in ("RGBA", "P"):
+            image = image.convert("RGB")
+        base_name = os.path.splitext(uploaded_file.name) [0]
+        save_path = f"{base_name}.jpg"
+
+        # 3. Save the image to the current working directory
+        image.save(save_path, "JPEG")
+        st.sidebar.success (f" Image successfully saved as `{save_path}'`!")
+    except Exception as e:
+        st.error(f"Error processing image: {e}")
+
 # ===========RESUME GENERATOR======================
 prompt = """Your are a helpful AI assistant
 with job resume maker, your task is to give
@@ -95,6 +124,15 @@ code, with professional design Format.
 User will upload data and return HTML format resume
 """  # Use different color or styling
 final_prompt = prompt + resume_maker_prompt()
+
+user_info = st.text_input("Enter your information")
+user_details = f"""user details: given below:
+Resume info: {user_info}
+Photo: {uploaded_file }
+Photo present ub current directory with name as
+uploaded_file, and once resume generated give
+dowload button in same html code.
+Default if not given: Give Python Developer Resume"""
 
 user_details = """User details: given below:
 Give Python Developer Resume
